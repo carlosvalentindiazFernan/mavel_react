91d8692dbc807186889c69d0199ee06294b09555
CarlsKe <fernancarldiaz@outlook.com>
2018-08-09 15:24:20 -0500

    feat: added initials fields

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react';
+import Button from '@material-ui/core/Button';
+import './App.css';
+
+
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <Button variant="contained" color="primary">
+          Hello World
+          </Button>
+      </div>
+    );
+  }
+}
+
+export default App;
